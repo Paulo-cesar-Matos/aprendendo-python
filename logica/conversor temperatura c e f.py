63bd4f078ab0f44c285d6fda24c791361f4fb6d7
Paulo Cesar Matos <matospcr0l4@gmail.com>
2026-09-17 20:20:58 -0300

    Create conversor temperatura c e f.py

---
+++ logica/conversor temperatura c e f.py
@@ -0,0 +1,11 @@
+while True:
+    print ("Qual a temperatura em fahrenheit? ")
+    temp_far = float
+    temp_celcius = (temp_far - 32) / 1.8
+    print("a temperatura em celcius é (temp_celcius)")            
+
+
+temp_far = input ("Qual a temperatura em fahrenheit? ")
+temp_celcius = (temp_far-32) / 1.8
+
+print("hello world")
