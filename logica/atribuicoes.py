fefafd8effdc00bb8412cb4eda19121bdd1709ae
Paulo Cesar Matos <matospcr0l4@gmail.com>
2026-09-21 08:43:33 -0300

    eu nao vou usar o pycharm 2

---
+++ logica/atribuicoes.py
@@ -0,0 +1,2 @@
+nome = input("Digite seu nome: ")
+print("Contempla, fagulha de consciência, a vastidão do Pleroma.", nome, "adentras as fronteiras do artífice, o arquiteto do cosmos visível e da matéria densa. Aqui, moldam-se as esferas celestes e forjam-se as ilusões geométricas do tempo e do espaço. Não há vinho, nem fogo, nem cadeiras; há apenas a matriz primordial da criação e o eco da palavra que organiza o caos em leis matemáticas.", nome, "és uma centelha divina aprisionada na minha obra de barro e carne.")# mostra o texto com a variavel, mensagem de saudação
--- logica/primeiro.py
+++ logica/primeiro.py
@@ -1,1 +1,4 @@
-print("nãocaravcnprecisacriarumscriptpramostrarnatelaolamundonexistessamaldiçãoétudoculpadoblosonaroedopteeuseiqntemnadavemasmsmeutocolocanopqeuqropodecolocarqlquercoisanoteuprimeirocódigosemerro👍👍👍👍👍👍👍👍👍")
+msg = "nãocaravcnprecisacriarumscriptpramostrarnatelaolamundonexistessamaldiçãoétudoculpadoblosonaroedopteeuseiqntemnadavemasmsmeutocolocanopqeuqropodecolocarqlquercoisanoteuprimeirocódigosemerro👍👍👍👍👍👍👍👍👍" # variavel que mostra a mensagem
+print("nãocaravcnprecisacriarumscriptpramostrarnatelaolamundonexistessamaldiçãoétudoculpadoblosonaroedopteeuseiqntemnadavemasmsmeutocolocanopqeuqropodecolocarqlquercoisanoteuprimeirocódigosemerro👍👍👍👍👍👍👍👍👍") # mostra apenas o texto
+print(msg) # mostra a variavel com o texto
+print("olha a mensagem: ", msg) # mostra o texto com a variavel
