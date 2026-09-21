6aaf3d8d8b0a94bce5e6d8ff934cc37687f7bb1c
Paulo Cesar Matos <matospcr0l4@gmail.com>
2026-09-21 17:00:59 -0300

    aaaaa

---
+++ logica/abc.py
@@ -0,0 +1,8 @@
+print("Maior, menor que ou igual a")
+val_a = int(input("Digite algum número: "))
+val_b = int(input("Digite algum outro número: "))
+
+if val_a > val_b :
+    print(f"O {val_a} é maior que {val_b}!")
+elif val_a < val_b :
+    print(f"O {val_a} é menor que {val_b}!")
